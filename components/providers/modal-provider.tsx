33c8a05fcda3a47af462d50dfc645a4b33550463
Christopher Haris <christoharis2005@gmail.com>
2024-05-06 19:30:29 +0700

    feat : edit server

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -3,22 +3,24 @@
 import { useEffect, useState } from "react";
 import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { InviteModal } from "@/components/modals/invite-modal";
+import { EditServerModal } from "@/components/modals/edit-server-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
-  
+
   useEffect(() => {
     setIsMounted(true);
   }, []);
 
   if (!isMounted) {
-    return null
+    return null;
   }
 
   return (
     <>
       <CreateServerModal />
-      <InviteModal/>
+      <InviteModal />
+      <EditServerModal />
     </>
   );
 };
