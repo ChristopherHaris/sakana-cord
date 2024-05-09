5607b8ac1f1963c11de656eb10bf7031b105f18c
Christopher Haris <christoharis2005@gmail.com>
2024-05-09 16:08:55 +0700

    feat : direct conversation

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,5 +1,6 @@
 import { Hash } from "lucide-react";
 import { MobileToggle } from "@/components/mobile-toggle";
+import { UserAvatar } from "@/components/user-avatar";
 
 interface ChatHeaderProps {
   serverId: string;
@@ -15,12 +16,15 @@ export const ChatHeader = ({
   imageUrl,
 }: ChatHeaderProps) => {
   return (
-    <div className="text-lg font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
-      <MobileToggle serverId={serverId}/>
+    <div className="font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
+      <MobileToggle serverId={serverId} />
       {type === "channel" && (
         <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
       )}
-      <p className="font-semibold text-lg text-black dark:text-white">{name}</p>
+      {type === "conversation" && (
+        <UserAvatar src={imageUrl} classname="w-8 h-8 md:h-8 md:w-8 mx-2" />
+      )}
+      <p className="font-semibold text-black dark:text-white">{name}</p>
     </div>
   );
 };
